a457fecd1263b95699cd010ea6f396c3bdbe45ce
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-11 11:12:27 -0700

    re-initialize tooltip when adding response input form

--- public/application.js
+++ public/application.js
@@ -1,7 +1,10 @@
 $('#response-btn').on('click', function() {
-  $('form .row').append(`<div class="input-field col s12">
-                          <i class="material-icons prefix">no_sim</i>
-                          <input id="icon_remove_response" type="tel" class="validate">
-                          <label for="icon_remove_response">Response</label>
-                         </div>`)
+  $('form .row').append(
+    `<div class="input-field col s12">
+      <i class="material-icons prefix tooltipped" data-position="bottom" data-delay="50" data-tooltip="Remove Response">no_sim</i>
+      <input id="icon_remove_response" type="tel" class="validate">
+      <label for="icon_remove_response">Response</label>
+     </div>`
+    )
+  $('.tooltipped').tooltip({delay: 50})
 })
