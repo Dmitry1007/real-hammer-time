c96401cee37586eb7ff5cb34b09dab2411a58ebe
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-11 11:48:13 -0700

    count number of dynamically added responses

--- public/application.js
+++ public/application.js
@@ -1,9 +1,11 @@
+var responseCount = 0
 $('#response-btn').on('click', function() {
+  responseCount++
   $('form .row').append(
     `<div class="input-field col s12">
-      <i class="material-icons prefix tooltipped" data-position="bottom" data-delay="50" data-tooltip="Remove Response">no_sim</i>
-      <input id="icon_remove_response" type="tel" class="validate">
-      <label for="icon_remove_response">Response</label>
+      <i class="material-icons prefix tooltipped" data-position="right" data-delay="50" data-tooltip="Remove Response">no_sim</i>
+      <input id="poll_response${responseCount}" type="tel" class="validate">
+      <label for="poll_response${responseCount}">Response</label>
      </div>`
     )
   $('.tooltipped').tooltip({delay: 50})
