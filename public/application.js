282a241c0488c4cf80ec29217efd776e2402b6ae
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-11 14:14:17 -0700

    click event listener on input icons and removal of input fields'

--- public/application.js
+++ public/application.js
@@ -1,12 +1,19 @@
 var responseCount = 0
-$('#response-btn').on('click', function() {
+$("#response-btn").on("click", function() {
   responseCount++
-  $('form .row').append(
-    `<div class="input-field col s12">
-      <i class="material-icons prefix tooltipped" data-position="right" data-delay="50" data-tooltip="Remove Response">no_sim</i>
+  $("form .row").append(
+    `<div class="new input-field col s12">
+      <i id="remove-response-icon" class="material-icons prefix tooltipped" data-position="right" data-delay="50" data-tooltip="Remove Response">no_sim</i>
       <input id="poll_response${responseCount}" type="tel" class="validate">
       <label for="poll_response${responseCount}">Response</label>
      </div>`
     )
-  $('.tooltipped').tooltip({delay: 50})
+  $(".tooltipped").tooltip({delay: 50})
 })
+
+$("form .row").on("click", function(event) {
+  if (event.target.parentElement.className === "new input-field col s12") {
+    $('.material-tooltip').remove()
+    event.target.parentElement.remove()
+  }
+});
