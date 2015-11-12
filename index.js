136f87597d81996eaffa010bbf353b1f5a72b0de
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-11 17:11:47 -0700

    require crypto and build out poll path

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 // Our Server
-pry = require('pryjs')
-const express = require('express');
+pry = require("pryjs")
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const path = require('path');
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
+const crypto = require('crypto');
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function (req, res){
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/", function (req, res){
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.post('/polls/new', function (req, res) {
+var polls = {}
+
+app.post("/polls/new", function (req, res) {
+  var poll      = req.body.poll;
+  var id        = crypto.randomBytes(10).toString("hex");
+
+  poll.id       = id;
+  poll.voterId  = crypto.randomBytes(10).toString("hex");
+  // poll.voterUrl = "/vote/" + voterId;
+
+  polls[id] = poll;
+  res.redirect("/polls/" + id);
+});
+
+app.get("/polls/:id", function (req, res) {
+  var poll = polls[req.params.id];
   // eval(pry.it)
-  res.redirect('/polls/new');
-});
-
-app.get('/polls/new', function (req, res) {
-  console.log("I'm here!")
+  res.send(`<html><body>
+      <h1>${poll.title}</h1>
+    </body></html>`);
 })
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('Your server is up and running on Port 3000. Good job!');
+  console.log("Your server is up and running on Port 3000. Good job!");
 });
