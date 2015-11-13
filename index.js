8db55dda256c1ce771ea9246b5161516426ea9d5
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-13 15:41:27 -0700

    add express helpers to project and use link_to for UserPollUrl in poll.ejs

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const Poll    = require("./models/poll")
 const _       = require("lodash")
 const pry     = require("pryjs")
 const express = require("express")
-const app     = express()
+var   app     = express()
 const http    = require("http").Server(app)
 const io      = require("socket.io")(http)
 const path    = require("path")
+
+var helpers = require('express-helpers')(app);
+
+app.set("view engine", "ejs")
 
 const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static("public"))
-
-app.set("view engine", "ejs")
 
 var polls = {}
 
