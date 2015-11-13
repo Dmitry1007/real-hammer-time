49489bdf15108c60acdf1f8031ce3478fe499436
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-13 09:53:14 -0700

    code style touch ups

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 // Server
 const Poll    = require("./models/poll")
-var _ = require('lodash');
-pry = require("pryjs")
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const path = require("path");
+const _       = require("lodash")
+const pry     = require("pryjs")
+const express = require("express")
+const app     = express()
+const http    = require("http").Server(app)
+const io      = require("socket.io")(http)
+const path    = require("path")
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-app.set('view engine', 'ejs')
-
-app.get("/", function (req, res){
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+app.set("view engine", "ejs")
 
 var polls = {}
 
-app.post("/poll/new", function (req, res) {
-  var poll = new Poll(req.body.poll)
+app.get("/", function (req, res){
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
-  polls[poll.id] = poll;
-  res.redirect("/poll/" + poll.id);
-});
+app.post("/poll/new", function (req, res) {
+  var poll       = new Poll(req.body.poll)
+  polls[poll.id] = poll
+  res.redirect("/poll/" + poll.id)
+})
 
 app.get("/poll/:id", function (req, res) {
-  var poll = polls[req.params.id];
-  res.render('poll', {pollData: poll})
+  var poll = polls[req.params.id]
+  res.render("poll", {pollData: poll})
 })
 
 app.get("/vote/:voterId", function (req, res) {
@@ -40,9 +39,9 @@ app.get("/vote/:voterId", function (req, res) {
   var poll = _.find(pollValues, function(value) {
     return value.voterId === req.params.voterId
   })
-  res.render('vote', {pollData: poll})
+  res.render("vote", {pollData: poll})
 })
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log("Your server is up and running on Port 3000. Good job!");
-});
+  console.log("Your server is up and running on Port 3000. Good job!")
+})
