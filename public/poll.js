6f72eabde6dedc3243c0c9988facc82e77d65641
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-16 10:42:16 -0700

    change poll title element on admin poll page from span to h4

--- public/poll.js
+++ public/poll.js
@@ -2,5 +2,4 @@ socket.on("clickVote", function (poll) {
   $('span').each(function(index, element) {
     element.innerText = poll.pollData.responsesAndVotes[element.id]
   })
-  $('.card-title').text(poll.pollData.title)
 });
