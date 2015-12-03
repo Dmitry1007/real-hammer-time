b27e3478b3dcbf2c4e2ff3f82a019b67b593b0ab
Dmitry Sky <dmitry.sky@icloud.com>
2015-12-03 15:59:09 -0700

    character counting for input fields and brute force check for empty input fields during form submition

--- public/add-response.js
+++ public/add-response.js
@@ -4,16 +4,28 @@ $("#response-btn").on("click", function() {
   $("form .row").append(
     `<div class="new input-field col s12">
       <i id="remove-response-icon" class="remove-icon material-icons prefix tooltipped" data-position="right" data-delay="50" data-tooltip="Remove Response">no_sim</i>
-      <input id="poll_response${responseCount}" type="tel" class="validate" name="poll[responses][]">
+      <input id="poll_response${responseCount} input_text" type="text" class="validate" name="poll[responses][]" length="20">
       <label for="poll_response${responseCount}">Response</label>
      </div>`
     )
   $(".tooltipped").tooltip({delay: 50})
+  $("input").characterCounter();
 })
 
 $("form .row").on("click", function(event) {
   if ($(event.target).hasClass('remove-icon')) {
     $('.material-tooltip').remove()
     event.target.parentElement.remove()
+  }
+});
+
+$("#form").submit(function() {
+  var pollTitle = document.getElementById("poll_title input_text").value
+  var pollQuestion = document.getElementById("poll_question input_text").value
+  var pollResponse = document.getElementById("poll_response input_text").value
+
+  if (pollTitle == "" || pollQuestion == "" || pollResponse == "") {
+      alert("you did not fill out one of the fields")
+      return false
   }
 });
