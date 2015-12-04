157fcbecd5e09779bed021bc9c3d4f91fdd6a94e
Dmitry Sky <dmitry.sky@icloud.com>
2015-12-03 17:56:20 -0700

    check browser before submitting boolean

--- public/form-submit.js
+++ public/form-submit.js
@@ -1,9 +1,11 @@
-// $("#form").submit(function() {
-//   var pollTitle = document.getElementById("poll_title input_text").value
-//   var pollQuestion = document.getElementById("poll_question input_text").value
-//   var pollResponse = document.getElementById("poll_response input_text").value
+if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+  $("#form").submit(function() {
+    var pollTitle = document.getElementById("poll_title input_text").value
+    var pollQuestion = document.getElementById("poll_question input_text").value
+    var pollResponse = document.getElementById("poll_response input_text").value
 
-//   if (pollTitle == "" || pollQuestion == "" || pollResponse == "") {
-//     return false
-//   }
-// });
+    if (pollTitle == "" || pollQuestion == "" || pollResponse == "") {
+      return false
+    }
+  });
+}
