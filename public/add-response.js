ab4af9546e8d9fcf101a2c906f2f37a2734c6ec4
Dmitry Sky <dmitry.sky@icloud.com>
2015-12-03 17:19:40 -0700

    add html5 required tag to input tag and eliminate 9-lines of pointless JS code, wow

--- public/add-response.js
+++ public/add-response.js
@@ -4,7 +4,7 @@ $("#response-btn").on("click", function() {
   $("form .row").append(
     `<div class="new input-field col s12">
       <i id="remove-response-icon" class="remove-icon material-icons prefix tooltipped" data-position="right" data-delay="50" data-tooltip="Remove Response">no_sim</i>
-      <input id="poll_response${responseCount} input_text" type="text" class="validate" name="poll[responses][]" maxlength="20">
+      <input required id="poll_response${responseCount} input_text" type="text" class="validate" name="poll[responses][]" maxlength="20">
       <label for="poll_response${responseCount}">Response</label>
      </div>`
     )
