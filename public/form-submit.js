892f7e20cf8e5f9f0ef98bfbca53db9ea5190037
Dmitry Sky <dmitry.sky@icloud.com>
2015-12-03 17:09:28 -0700

    change materialize length to html maxlength

---
+++ public/form-submit.js
@@ -0,0 +1,9 @@
+$("#form").submit(function() {
+  var pollTitle = document.getElementById("poll_title input_text").value
+  var pollQuestion = document.getElementById("poll_question input_text").value
+  var pollResponse = document.getElementById("poll_response input_text").value
+
+  if (pollTitle == "" || pollQuestion == "" || pollResponse == "") {
+    return false
+  }
+});
